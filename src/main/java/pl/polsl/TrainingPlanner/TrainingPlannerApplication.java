ab7e743b15c6964a6cb19d4a8d000610fc6cf6cc
j124klo <cioskradek@gmail.com>
2026-04-03 14:31:19 +0200

    Initial commit

---
+++ src/main/java/pl/polsl/TrainingPlanner/TrainingPlannerApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.TrainingPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/polsl/TrainingPlanner/TrainingPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.TrainingPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
